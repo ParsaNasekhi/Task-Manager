d9dff79cf0833d125d474356028f6834c196f932
ParsaNasekhi <parsa3739nasekhi@gmail.com>
2024-02-05 19:57:35 +0330

    listsPage: init this page

--- lib/screens/lists_page.dart
+++ lib/screens/lists_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+
+import '../db/list/list_data.dart';
 
 class ListsPage extends StatefulWidget {
   const ListsPage({super.key});
@@ -8,6 +11,9 @@ class ListsPage extends StatefulWidget {
 }
 
 class _ListsPageState extends State<ListsPage> {
+  final TextEditingController _listNameController = TextEditingController();
+  final Box<ListData> listBox = Hive.box<ListData>("ListBox");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,11 +22,79 @@ class _ListsPageState extends State<ListsPage> {
         title: const Text("Lists"),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) =>
+                  StatefulBuilder(builder: (context, statSetter) {
+                    return AlertDialog(
+                        actionsAlignment: MainAxisAlignment.spaceEvenly,
+                        actions: [
+                          ElevatedButton(
+                              onPressed: () {
+                                if (_listNameController.text.isNotEmpty) {
+                                  setState(() {
+                                    insertNewList(_listNameController.text);
+                                    _listNameController.text = "";
+                                    Navigator.of(context).pop();
+                                  });
+                                }
+                              },
+                              child: const Text("Add")),
+                          ElevatedButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text("Cancel")),
+                        ],
+                        title: const Text('Add New List'),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TextField(
+                              controller: _listNameController,
+                              decoration: const InputDecoration(
+                                labelText: "List Name",
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                          ],
+                        ));
+                  }));
+        },
+        child: const Icon(Icons.add),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Wrap(
+          children: List.generate(listBox.length, (index) {
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width / 2 - 64,
+                  height: MediaQuery.of(context).size.width / 2 - 64,
+                  child: Card(
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Text(listBox.values.toList()[index].listName, style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 20), maxLines: 3, overflow: TextOverflow.ellipsis,),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            }),
+        ),
       ),
     );
+  }
+}
+
+void insertNewList(String name) {
+  final ListData listData = ListData()
+    ..listName = name;
+  if (!listData.isInBox) {
+    final Box<ListData> box = Hive.box<ListData>("ListBox");
+    box.add(listData); // insert
   }
 }
